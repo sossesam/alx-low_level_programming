f8865b46bcf05f4ed274218df95891ab4970d82d
sossesam <sossesam@gmail.com>
2022-09-10 07:14:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/* Using put to display the text */
+/**
+ *  * main - prints the phrase using put 
+ *   * Return: 0 if exited properly, Because we used the pameter void
+ *    */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
