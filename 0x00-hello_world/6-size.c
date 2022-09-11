24625058d1e3301e92c7767539b0405ca84c8fa4
sossesam <sossesam@gmail.com>
2022-09-10 22:05:00 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@ int main () {
 	printf("Size of int: %zu bytes\n", sizeof(int));
 	printf("Size of float: %zu bytes\n", sizeof(float));
 	printf("Size of char: %zu byte\n", sizeof(char));
-	printf("Size of lonng long: %d byte\n", sizeof(long long));
+	printf("Size of long long: %d byte\n", sizeof(long long));
 
 	return 0;
 }
