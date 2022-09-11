a7c73d3f168f5a6da9d1bbdaaa48141d7930f210
sossesam <sossesam@gmail.com>
2022-09-10 22:00:53 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 int main () {
-	printf("Size of long:%d bytes\n", sizeof(long))
+	printf("Size of long:%d bytes\n", sizeof(long));
 	printf("Size of int: %zu bytes\n", sizeof(int));
 	printf("Size of float: %zu bytes\n", sizeof(float));
 	printf("Size of char: %zu byte\n", sizeof(char));
