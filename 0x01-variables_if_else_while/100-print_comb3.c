205e9ef18fd855c92b5b5821810c7db3a09536b4
oyeniran samuel sanmi <sossesam@gmail.com>
2022-09-13 22:21:54 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,39 +9,39 @@
  * 01 and 10 are considered the same combination of the two digits 0 and 1
  * Print only the smallest combination of two digits
  * Numbers should be printed in ascending order, with two digits
- *          * You can only use the putchar function
- *           * (every other function (printf, puts, etc…) is forbidden)
- *            * You can only use putchar five times maximum in your code
- *             * You are not allowed to use any variable of type char
- *              * All your code should be in the main function
- *               * Return: 0
- *                */
+ * You can only use the putchar function
+ * (every other function (printf, puts, etc…) is forbidden)
+ * You can only use putchar five times maximum in your code
+ * You are not allowed to use any variable of type char
+ * All your code should be in the main function
+ * Return: 0
+ */
 int main(void)
 {
-		int c;
-			int d = 0;
+	int c;
+	int d = 0;
 
-				while (d < 10)
-						{
-									c = 0;
-											while (c < 10)
-														{
-																		if (d != c && d < c)
-																						{
-																											putchar('0' + d);
-																															putchar('0' + c);
+	while (d < 10)
+	{
+		c = 0;
+		while (c < 10)
+		{
+			if (d != c && d < c)
+			{
+				putchar('0' + d);
+				putchar('0' + c);
 
-																																			if (c + d != 17)
-																																								{
-																																														putchar(',');
-																																																			putchar(' ');
-																																																							}
-																																						}
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 
-																					c++;
-																							}
-													d++;
-														}
-					putchar('\n');
-						return (0);
+			c++;
+		}
+		d++;
+	}
+	putchar('\n');
+	return (0);
 }
