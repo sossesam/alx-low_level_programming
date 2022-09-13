a01e561ea06f893c0fc29a52837a944e17c93d00
sossesam <sossesam@gmail.com>
2022-09-13 13:03:05 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,16 +6,17 @@
  * starting from 0, followed by a new line.
  * Return: 0
  */
-int main()
+int main(void)
 {
 	int c;
+
 	c = 0;
 
-	while (c <= 16)
+	while (c < 16)
 	{
-		printf("%x",c);
+		printf("%x", c);
 		c++;
 	}
 	printf("\n");
-	return(0);
+	return (0);
 }
