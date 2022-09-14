e7cc0db601008dd7c082ce144a90dbf8d7e7785b
sossesam <sossesam@gmail.com>
2022-09-14 08:52:09 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- *main - description should be here
+ * main - check the code for Holberton School students.
  *
- *Return: should be here
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	int c = 0;
-
+	print_alphabet();
+	return (0);
 }
