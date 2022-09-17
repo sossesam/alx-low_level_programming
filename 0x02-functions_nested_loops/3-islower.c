041ddbd81a48258b83c61b56679c5142c663975b
sossesam <sossesam@gmail.com>
2022-09-17 11:54:27 -0700

    main.h

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,6 +4,8 @@
  * _islower - function to detect lower case
  *
  * Return: returns 0
+ *
+ * c: the parameter c is the ascii number 
  */
 
 int _islower(int c)
@@ -11,9 +13,8 @@ int _islower(int c)
 	if (c >= 97 && c <= 122)
 	{
 		return (1);
-	}else
-	{
-		return (0);
 	}
+
+	return (0);
 
 }
