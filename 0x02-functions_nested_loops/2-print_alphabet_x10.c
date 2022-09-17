08e6629e531fbf078197cb21aa4c9f41c5b0e75d
sossesam <sossesam@gmail.com>
2022-09-17 04:05:20 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet_x1 - the functionn prints alphabets 10 times
+ * print_alphabet_x10 - the functionn prints alphabets 10 times
  *
- * Return-returns 0
+ * Return:returns 0
  */
 
 void print_alphabet_x10(void)
