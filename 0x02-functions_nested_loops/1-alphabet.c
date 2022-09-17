d6c09afef2f57a213b6d92675331a7d775324044
sossesam <sossesam@gmail.com>
2022-09-17 02:04:56 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,18 @@
 #include "main.h"
-
+/**
+ * print_alphabet - fuctionn that prints alphabet from a to z
+ *
+ * Return- its a void so no return parameter
+ */
 
 
 void print_alphabet(void)
 {
 	char c;
 
-	while (c <='z')
+	c = 'a';
+
+	while (c <= 'z')
 	{
 		_putchar(c);
 		c++;
