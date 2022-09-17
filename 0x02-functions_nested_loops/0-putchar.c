2ba4d58264b658058848c9465a5c40a9bf5cef9f
sossesam <sossesam@gmail.com>
2022-09-17 00:05:00 -0700

    main.h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main- a function that prints
+ *
+ * Return:  returns 0
+ */
+
+int main(void)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+
+	return (0);
+
+}
+
+
