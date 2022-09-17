f0b66f9fa7bd20fb4a6d701ec5e8ad31692c9c16
sossesam <sossesam@gmail.com>
2022-09-17 13:53:14 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - funnction that returns alphabet
+ *
+ * @c: return 0 or 1
+ *
+ * Return: returns 0 or 1
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+
+	return (0);
