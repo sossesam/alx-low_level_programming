e0e2f9d80a97b2d8ab4d11fe773bdd48b02dc63d
sossesam <sossesam@gmail.com>
2022-09-18 02:20:55 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -36,7 +36,7 @@ void times_table(void)
 				_putchar((result / 10) + '0');
 				_putchar((result % 10) + '0');
 			}
-			if (horizontal < 10)
+			if (horizontal < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
