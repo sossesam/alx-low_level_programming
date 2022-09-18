65b10ff3e68223dcb959b0c4ce163b57c4c71e39
sossesam <sossesam@gmail.com>
2022-09-18 08:55:23 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,47 +8,42 @@
 
 void print_times_table(int n)
 {
-	int vertical;
-	int horizontal;
+	int i, j, k;
 
-	vertical = 0;
-
-	while (vertical <= n)
+	if (n >= 0 && n <= 15)
 	{
-
-		if (n < 0 || n > 15)
+		for (i = 0; i <= n; i++)
 		{
-			break;
+			for (j = 0; j <= n; j++)
+			{
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
 		}
-		horizontal = 0;
-		while (horizontal <= n)
-		{
-			int result;
-
-			result = vertical * horizontal;
-
-			if (result == 0 && horizontal == 0)
-			{
-				_putchar(result + '0');
-			}
-			else if (result < 10 && horizontal > 0)
-			{
-				_putchar(' ');
-				_putchar('0' + result);
-			}
-			else if (result >= 10 )
-			{
-				_putchar((result / 10) + '0');
-				_putchar((result % 10) + '0');
-			}
-			if (horizontal < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			horizontal++;
-		}
-		_putchar('\n');
-		vertical++;
 	}
 }
