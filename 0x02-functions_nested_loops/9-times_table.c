c682b16dad51e30b20fc624060f5736bd86c3c10
sossesam <sossesam@gmail.com>
2022-09-18 01:05:16 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * times_table - prints times table
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int vertical;
+	int horizontal;
+
+	
+	vertical = 0;
+	horinzontal = 0;
+
+
+	while (vertical < 10)
+	{
+		while (horizontal < 10)
+		{
+			int result;
+
+			result = vertical * horizontal;
+
+			_putchar(result)
+
+			if (horizontal < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			horizontal++;
+
+		}
+
+		_putchar('\n');
+		vertical++;
+	}
+}
