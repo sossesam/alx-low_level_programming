4047a4c7de9861829323a0c3a74b221c2fdd907a
sossesam <sossesam@gmail.com>
2022-09-18 02:40:57 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - here we describe the funnction
+ *
+ * return: description for the result
+ *
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return(result);
+}
+
