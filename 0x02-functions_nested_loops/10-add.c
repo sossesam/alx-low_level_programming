b4d8158892b46d2d7df25883bdd9c8f8236a6bab
sossesam <sossesam@gmail.com>
2022-09-18 02:47:35 -0700

    main.h

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,9 +4,8 @@
  * add - here we describe the funnction
  * @a: first integer
  * @b: second  integer
- * @result: result
  *
- * return: description for the result
+ * Return: description for the result
  *
  */
 
