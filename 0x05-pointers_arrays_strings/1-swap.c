7d504bd67403c87338b5c76832d5a3544c541f23
sossesam <sossesam@gmail.com>
2022-09-20 16:23:20 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
+/**
+ * swap_int- funnction to swap variabbles
+ *
+ * Return: returns a funnction
+ */
+
 void swap_int(int *a, int *b)
 {
-	
 	int c;
 
 	c = *a;
 	*a = *b;
 	*b = c;
-
-
-
-
-
-
-
 }
