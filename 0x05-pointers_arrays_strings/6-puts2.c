ece82394e5f33c85f8a14f2eac91639838a45643
oyeniran samuel sanmi <sossesam@gmail.com>
2022-10-01 18:49:03 +0100

    6puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * puts2 - prints one char out of 2 of a string
  * followed by a new line
@@ -12,10 +13,14 @@ void puts2(char *str)
 
     while(str[len] != '\0')
     {
+
         len++;
+
     }
     for(i = 0; i <= len; i += 2)
     {
+
         _putchar(str[i]);
+        
     }
 }
