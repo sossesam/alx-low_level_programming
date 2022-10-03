42fe1d7259a06a59337a12b129e7d87ca92b7ade
oyeniran samuel sanmi <sossesam@gmail.com>
2022-10-03 09:26:30 +0100

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,10 +8,11 @@
  * Return: 0
  */
 
-int main(int argc , char* argv[])
+int main(int argc, char *argv[])
 {
-    printf("%s\n", argv[0]);
     (void)argc;
 
-    return 0;
+    printf("%s\n", argv[0]);
+
+    return (0);
 }
