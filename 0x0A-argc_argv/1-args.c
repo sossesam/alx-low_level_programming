849ec471f3a8d8f83da39956533e7153c474de7b
oyeniran samuel sanmi <sossesam@gmail.com>
2022-10-03 10:17:40 +0100

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints number of parameter followe dby a new line;
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+    (void)argv;
+    printf("%d\n", argc);
+
+    return 0;
+}
