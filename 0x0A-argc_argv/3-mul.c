1f998ca91612e22b621c27bc4083e93979ab329d
oyeniran samuel sanmi <sossesam@gmail.com>
2022-10-03 18:39:33 +0100

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int x, y, result;
+    if(argc != 3)
+    {
+        printf("Error\n");
+        return(1);
+    }
+
+    x = atoi(argv[1]);
+    y = atoi(argv[2]);
+
+    result = x * y;
+
+    printf("%d\n", result);
+
+    return (0);
+
+
+}
