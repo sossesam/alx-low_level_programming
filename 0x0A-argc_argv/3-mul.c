6a276824aa7a10144ddb02bc1d5456e590052f28
sossesam <sossesam@gmail.com>
2022-10-16 02:47:46 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,26 +4,24 @@
 
 /**
  * _atoi - converts a string to an integer
- * @s: string to be converted
+ * @argc: string to be converted
+ * @argv: pointer to string
  *
  * Return: the int converted from the string
  */
 
 int main(int argc, char *argv[])
 {
-    int x, y, result;
-    if(argc != 3)
-    {
-        printf("Error\n");
-        return(1);
-    }
+	int x, y, result;
 
-    x = atoi(argv[1]);
-    y = atoi(argv[2]);
-
-    result = x * y;
-
-    printf("%d\n", result);
-
-    return (0);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	result = x * y;
+	printf("%d\n", result);
+	return (0);
 }
