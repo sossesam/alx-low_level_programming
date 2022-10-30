609163c9c41382abc1b9a7e97d20cca3c96175ec
oyeniran samuel sanmi <sossesam@gmail.com>
2022-10-30 09:49:29 +0100

    101-quote.c

--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -1,19 +1,20 @@
 	.section	__TEXT,__text,regular,pure_instructions
 	.build_version macos, 10, 15	sdk_version 10, 15, 6
+	.intel_syntax noprefix
 	.globl	_main                   ## -- Begin function main
 	.p2align	4, 0x90
 _main:                                  ## @main
 	.cfi_startproc
 ## %bb.0:
-	pushq	%rbp
+	push	rbp
 	.cfi_def_cfa_offset 16
-	.cfi_offset %rbp, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register %rbp
-	xorl	%eax, %eax
-	movl	$0, -4(%rbp)
-	popq	%rbp
-	retq
+	.cfi_offset rbp, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register rbp
+	xor	eax, eax
+	mov	dword ptr [rbp - 4], 0
+	pop	rbp
+	ret
 	.cfi_endproc
                                         ## -- End function
 .subsections_via_symbols
