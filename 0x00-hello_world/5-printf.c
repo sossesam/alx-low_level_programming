5ff0750878ae66dce0114f78c44225f629d6c80b
oyeniran samuel sanmi <sossesam@gmail.com>
2022-10-30 07:09:50 +0100

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+    printf("with proper grammar, but the outcome is a piece of art,");
+
+    return(0);
+}
