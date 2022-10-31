81847c060d94bb52f0a2607b8e63c5166e7721ff
oyeniran samuel sanmi <sossesam@gmail.com>
2022-10-31 07:47:56 +0100

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char number;
+
+    for(number = '0'; number <= '9'; number++)
+    {
+       
+            putchar(number);
+
+    }
+     putchar('\n');
+
+     return (0);
+}
