76a9ec285ca0866ef456c73fd0313971d0032025
oyeniran samuel sanmi <sossesam@gmail.com>
2022-10-31 08:13:58 +0100

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char alphabet;
+
+    for(alphabet = 'z'; alphabet >= 'a'; alphabet--)
+    {
+        putchar(alphabet);
+    }
+    putchar('\n');
+
+     return (0);
+}
