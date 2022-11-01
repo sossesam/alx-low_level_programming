b1aa453dbc76ca1d083bb4ed6cdf171af71ab601
oyeniran samuel sanmi <sossesam@gmail.com>
2022-11-01 22:50:37 +0100

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void){
+    int hundred, tens, unit, sum;
+    for(hundred = 0; hundred < 8; hundred++){
+        tens = hundred + 1;
+        while(tens < 9){
+            unit = tens + 1;
+            while(unit <= 9){
+                sum = hundred + tens + unit;
+                putchar(hundred + '0');
+                putchar(tens + '0');
+                putchar(unit + '0');
+                if(sum < 24){
+                    putchar(',');
+                    putchar(' ');
+                }
+                
+                unit++;
+                }
+                tens++;
+            }
+        }
+        putchar('\n');
+    }
