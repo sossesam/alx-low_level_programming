b6733897067d21996105423eb1fb380db3c5dbb0
oyeniran samuel sanmi <sossesam@gmail.com>
2022-11-01 22:59:42 +0100

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 
-int main(void){
+int main(void)
+{
     int hundred, tens, unit, sum;
-    for(hundred = 0; hundred < 8; hundred++){
+    for(hundred = 0; hundred < 8; hundred++)
+    {
         tens = hundred + 1;
-        while(tens < 9){
+        while(tens < 9)
+        {
             unit = tens + 1;
-            while(unit <= 9){
+            while(unit <= 9)
+            {
                 sum = hundred + tens + unit;
                 putchar(hundred + '0');
                 putchar(tens + '0');
                 putchar(unit + '0');
-                if(sum < 24){
+                if(sum < 24)
+                {
                     putchar(',');
                     putchar(' ');
                 }
                 
                 unit++;
-                }
-                tens++;
             }
+                tens++;
         }
-        putchar('\n');
     }
+        putchar('\n');
+}
