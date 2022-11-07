d8620aa0506d5a2df93770b7ac133a9797730523
oyeniran samuel sanmi <sossesam@gmail.com>
2022-11-07 09:33:49 +0100

    9-times_table.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+void print_to_98(int n)
+{
+    if(n < 98)
+    {
+        while(n < 98)
+        {
+            if(n <= 9)
+            {
+                _putchar(' ');
+                _putchar('0' + n);
+                _putchar(',');
+            }
+            
+            if(n >= 9)
+            {
+                _putchar(' ');
+                _putchar('0' + (n / 10));
+                _putchar('0' + (n % 10));
+                _putchar(',');
+            }
+            n++;
+        }
+        _putchar(' ');
+         _putchar('9');
+        _putchar('8');
+        _putchar('\n');
+    }else if(n >= 98)
+    {
+        while(n > 98)
+        {
+             if(n > 98 && n < 100)
+            {
+                _putchar(' ');
+                _putchar('0' + (n / 10));
+                _putchar('0' + (n % 10));
+                _putchar(',');
+            }else if(n > 98 && n >= 100)
+            {
+                _putchar(' ');
+                _putchar('0' + (n / 100));
+                _putchar('0' + ((n - 100 )/ 10));
+                _putchar('0' + (n % 10));
+                _putchar(',');
+            }else if(n == 98)
+            {  _putchar('9');
+               _putchar('8');
+               _putchar('\n');}
+            n--;
+        }
+    }
+}
