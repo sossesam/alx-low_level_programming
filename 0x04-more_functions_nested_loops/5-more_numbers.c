e81da72b8db3e9b17702415797c4f41b82c4ce1c
oyeniran samuel sanmi <sossesam@gmail.com>
2022-11-08 20:01:15 +0100

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,17 +4,16 @@ void more_numbers(void)
 {
     int units,count;
 
-    for(count = 0; count <= 10; count++)
+    for(count = 0; count < 10; count++)
     {
         for(units = 0; units < 15; units++)
         {
             if(units >= 10)
             {
                 _putchar((units / 10) + '0');
-                _putchar((units % 10) + '0');
-            }else{
-                _putchar(units + '0');
             }
+                _putchar((units % 10)+ '0');
+            
             
         }
         _putchar('\n');
