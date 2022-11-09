f6bfcd6e2c030c6f0294192787b8542116936393
oyeniran samuel sanmi <sossesam@gmail.com>
2022-11-09 20:12:49 +0100

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    for(x = 1; x <= 100; x++){
+        if(x % 15 == 0){
+            printf("FizzBuzz ");
+        }
+        else if(x % 5 == 0){
+            printf("Buzz ");
+        }else if(x % 3 == 0){
+            printf("Fizz ");
+        }else{
+            printf("%d ",x);
+        }
+        
+    }
+    return (0);
+}
