1614c38c4ed5942e6a8664d698fe953dfeb2475a
oyeniran samuel sanmi <sossesam@gmail.com>
2022-11-09 18:29:15 +0100

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+void print_diagonal(int n)
+{
+    int x,y;
+    x = 0;
+    
+    
+    while(x < n && n > 0)
+    {
+        y = 0;
+        while(y < x)
+        {
+            _putchar(' ');
+            y++;
+
+        }
+
+        _putchar(92);
+        _putchar('\n');
+        
+
+        x++;
+    }
+}
