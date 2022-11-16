954410d3ea8949df071ec3b19b41eaa98db0da32
oyeniran samuel sanmi <sossesam@gmail.com>
2022-11-16 19:38:11 +0100

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include<stdio.h>
 
 
 void puts_half(char *str)
@@ -14,7 +15,7 @@ void puts_half(char *str)
    if (length % 2 == 0){
     start = length / 2;
    }else{
-    start = (length - 1)/2;
+    start = (length + 1)/2;
    }
 
    for(c = 0; c < length; c++){
