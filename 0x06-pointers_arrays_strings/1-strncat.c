d8682eef1a51bd4a08fc4413df87c7dbe666c8e8
oyeniran samuel sanmi <sossesam@gmail.com>
2022-11-19 21:03:17 +0100

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+    int a;
+    int b;
+
+    b = 0;
+    while(*(dest + b) != '\0')
+    {
+        b++;
+    }
+
+    for(a = 0; a < n && *(src + a) != '\0'; a++)
+    {
+        *(dest + b) = *(src + a);
+        b++;
+    }
+    *(dest + b) = '\0';
+
+    return(dest);
+}
