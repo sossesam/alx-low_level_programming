8e3fda12dea566cc131f714b6a2d9768d8d4036a
oyeniran samuel sanmi <sossesam@gmail.com>
2022-12-01 04:29:14 +0100

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+    unsigned int i;
+    i = 0;
+
+  
+        while(i < n)
+        {
+            *(s + i) = b;
+            i++;
+        }
+        
+    
+    return(s);
+}
