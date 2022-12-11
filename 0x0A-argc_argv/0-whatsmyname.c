931f9a1fd2743df5a61ca8125ad85932d431c220
oyeniran samuel sanmi <sossesam@gmail.com>
2022-12-11 21:36:24 +0100

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+int main(int argc, char * argv[])
+{
+    int count;
+    count = argc;
+    printf("%s \n", argv[0]);
+    return(0);
+}
