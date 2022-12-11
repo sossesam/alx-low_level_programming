52ee8b0aa3bd83329ca1dfc35b501e303bada63d
oyeniran samuel sanmi <sossesam@gmail.com>
2022-12-11 21:44:27 +0100

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - print its name
+ * @argc: argument count
+ * @argv: argument vector for values
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char * argv[])
+{
+    (void) argv;
+    printf("%d\n", argc);
+    return(0);
+}
