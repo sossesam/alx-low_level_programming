6d2709aa52ad37f9ba8fe9adf5aa6c8a8c139a00
oyeniran samuel sanmi <sossesam@gmail.com>
2022-12-15 08:35:48 +0100

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,15 +7,15 @@ char *_strdup(char *str)
     int count;
     int i;
     count = 0;
-    while(str[count] != '\0')
+    if(str == NULL)
+    {
+        string = NULL;
+    }
+    else {
+        while(str[count] != '\0')
     {
         count = count + 1;
     }
-    if(str == NULL)
-    {
-        return NULL;
-    }
-    else {
         string = malloc(sizeof(char) * (count + 1));
         if(count == 0)
         {
