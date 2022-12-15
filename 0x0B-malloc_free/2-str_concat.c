04120a00140225d1f7b38839f369020368eeeebc
oyeniran samuel sanmi <sossesam@gmail.com>
2022-12-15 16:06:31 +0100

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *str_concat(char *s1, char *s2)
+{
+    char *array;
+    int len_s1;
+    int len_s2;
+    int t_len;
+    int i;
+    int j;
+    int k;
+    int l;
+    len_s1 = 0;
+    len_s2 = 0;
+    t_len = len_s1 + len_s2;
+    
+    for (i = 0; s1[i] != '\0'; i++)
+    {
+        len_s1 = i;
+    }
+     for (j = 0; s2[j] != '\0'; j++)
+    {
+        len_s2 = j;
+    }
+
+    array = malloc(sizeof(char) * (t_len + 2));
+    for ( l = 0; l < len_s1 + 1; l++)
+    {
+        array[l] =  s1[l];
+    }
+    for ( k = 0; k < len_s2 + 1; k++)
+    {
+        array[k + l] =  s2[k];
+    }
+return array;
+}
