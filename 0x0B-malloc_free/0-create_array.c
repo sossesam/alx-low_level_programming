006adf67a41fe33b5eb4b3b056581909608a1e7c
oyeniran samuel sanmi <sossesam@gmail.com>
2022-12-15 06:15:33 +0100

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
         array = malloc(sizeof(char) * size);
         if (size == 0)
 			{
-				array[size] = '\0';
+				return NULL;
 			}
             else
 			{
