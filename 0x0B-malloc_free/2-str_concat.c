de89a81aaa26c7d0760a8bf4569a1b84e1526c76
oyeniran samuel sanmi <sossesam@gmail.com>
2022-12-15 16:28:48 +0100

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,17 +4,11 @@
 char *str_concat(char *s1, char *s2)
 {
     char *array;
-    int len_s1;
-    int len_s2;
-    int t_len;
-    int i;
-    int j;
-    int k;
-    int l;
+    int len_s1, len_s2, t_len, i, j, k, l;
     len_s1 = 0;
     len_s2 = 0;
     t_len = len_s1 + len_s2;
-    
+
     for (i = 0; s1[i] != '\0'; i++)
     {
         len_s1 = i;
@@ -24,7 +18,9 @@ char *str_concat(char *s1, char *s2)
         len_s2 = j;
     }
 
-    array = malloc(sizeof(char) * (t_len + 2));
+    array = malloc(sizeof(char) * (t_len));
+
+
     for ( l = 0; l < len_s1 + 1; l++)
     {
         array[l] =  s1[l];
@@ -33,5 +29,6 @@ char *str_concat(char *s1, char *s2)
     {
         array[k + l] =  s2[k];
     }
+    array[k + l] = '\0';
 return array;
 }
