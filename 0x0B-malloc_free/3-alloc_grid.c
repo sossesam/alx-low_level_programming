0136ff468b85c4972a7bafcc524875bcaba9d126
oyeniran samuel sanmi <sossesam@gmail.com>
2022-12-18 06:12:01 +0100

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -6,11 +6,30 @@ int **alloc_grid(int width, int height)
 {
     int **first_step;
     int i, j;
+
+    if(width <= 0|| height <= 0)
+    {
+        return NULL;
+    }
+
     first_step = malloc(sizeof(int *) * width);
+    if(first_step == NULL)
+    {
+        return NULL;
+    }
 
     for(i = 0; i < width; i++)
     {
         first_step[i] = malloc(sizeof(int) * height);
+        if (first_step[i] == NULL)
+		{
+			for (i = i - 1; i >= 0 ; i--)
+			{
+				free(first_step[i]);
+			}
+			free(first_step);
+			return (NULL);
+		}
         for(j = 0; j < height; j++)
         {
             first_step[i][j] = 0;
