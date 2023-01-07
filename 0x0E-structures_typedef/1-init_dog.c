fad2ca92787371b9136d9b4acfc3e4032beb69b5
oyeniran samuel sanmi <sossesam@gmail.com>
2023-01-07 19:27:08 +0100

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,9 @@
+#include "dog.h"
+#include <stdlib.h>
+
+void init_dog(struct dog *d, char *name, float age, char *owner){
+    
+    d -> name = name;
+    (*d).age = age;
+    (*d).owner = owner;
+}
