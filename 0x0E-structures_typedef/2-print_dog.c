4f74ec71b5e3f9a70fbca9d71630bdbc40909fdf
oyeniran samuel sanmi <sossesam@gmail.com>
2023-01-07 20:43:24 +0100

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_dog(struct dog *d){
+if(d->name == NULL){
+d->name = "nil";
+}
+printf("Name: %s\n", d->name);
+printf("Age: %.6lf\n", d->age);
+if(d->owner == NULL){
+ d->owner = "nil";
+}
+printf("Owner: %s\n", d->owner);
+}
