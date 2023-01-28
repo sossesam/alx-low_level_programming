c2b2843227d639933e71f49d279fff3f842b1e1d
oyeniran samuel sanmi <sossesam@gmail.com>
2023-01-28 08:15:47 +0100

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,7 +14,7 @@ void print_numbers(const char *separator, const unsigned int n, ...){
     for(i =  0; i < n; i++){
 
         printf("%d", va_arg(param, int));
-        if(i < n - 1){
+        if(i < n - 1 && separator != 0){
              printf("%c ", *separator);
         }
        
