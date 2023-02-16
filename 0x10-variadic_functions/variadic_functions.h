1d6a3e798e5225358920dbdb8ccafd11dcffe2be
oyeniran samuel sanmi <sossesam@gmail.com>
2023-02-16 09:14:25 +0100

    all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,15 @@
 #ifndef VARIADIC_FUNCTION_H
 #define VARIADIC_FUNCTION_H
 
+typedef struct what_format
+{
+	char *type;
+	void (*f)();
+} what_format;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
