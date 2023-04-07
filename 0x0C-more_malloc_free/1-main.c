ca83b21b6182da5aa8a99f9e0588138b22f4c413
oyeniran samuel sanmi <sossesam@gmail.com>
2023-04-07 14:28:33 +0100

    another

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -11,7 +11,7 @@ int main(void)
 {
     char *concat;
 
-    concat = string_nconcat("Best ", "school !!!", 5);
+    concat = string_nconcat("Best ", "school !!!", 9);
     printf("%s\n", concat);
     free(concat);
     return (0);
