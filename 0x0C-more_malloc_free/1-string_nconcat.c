e8d56d6f9f191c9b7abb1e3281d95bfd3f296b6c
oyeniran samuel sanmi <sossesam@gmail.com>
2023-04-07 14:22:40 +0100

    all

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,49 +1,54 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * string_nconcat - function that concatenates two strings
+ * @s1: string one being passed
+ * @s2: string two being passed
+ * @n: size
+ * Return: Always 0.
+ */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *s3;
-    unsigned int length_s1;
-    unsigned int length_s2;
-    unsigned int i;
-    int count;
-    count = 0;
-    if(s2 == NULL){
-        s2 = malloc(sizeof (char));
-        s2 = "";
-    }
-    if(s1 == NULL){
-        s1 = malloc(sizeof (char));
-        s1 = "";
-    }
+char *s3;
+unsigned int length_s1;
+unsigned int length_s2;
+unsigned int a, b, c, d;
+int count;
 
-    for(i = 0; s1[i] != '\0'; i++){
-        length_s1 = i + 1;
-    }
-    for(i = 1; s2[i] != '\0'; i++){
-        length_s2 = i;
-    }
+count = 0;
+if (s2 == NULL)
+{
+s2 = malloc(sizeof(char));
+s2 = "";
+}
+if (s1 == NULL)
+{
+s1 = malloc(sizeof(char));
+s1 = "";
+}
+for (a = 0; s1[a] != '\0'; a++)
+length_s1 = a;
 
-    if (n > length_s2)
-		s3 = malloc((length_s1 + length_s2 + 1) * sizeof(char));
-	else
-		s3 = malloc((length_s1 + n + 1) * sizeof(char));
+for (b = 1; s2[b] != '\0'; b++)
+length_s2 = b;
 
-    if(s3 == NULL){
-        return NULL;
-    }
+if (n > length_s2)
+s3 = malloc((length_s1 + length_s2 + 1) * sizeof(char));
+else
+s3 = malloc((length_s1 + n + 1) * sizeof(char));
+if (s3 == NULL)
+return (NULL);
 
-    
-    for(i = 0; i <= length_s1; i++){
-        s3[i] = s1[i];
-        count = i;
-    }
+for (c = 0; c <= length_s1; c++)
+s3[c] = s1[c];
 
-    for(i = 0; i <= n; i++){
-        s3[count + i] = s2[i];
-    }
-    s3[length_s1 + length_s2] = '\0';
-    return s3;
+for (d = 0; d <= n && d <= b ; d++, c++)
+{
+s3[c] = s2[d];
+}
+
+s3[c] = '\0';
+return (s3);
 }
