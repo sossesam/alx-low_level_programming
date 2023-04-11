aafa0956ea95c9adc3e11789a34953d123bad39d
oyeniran samuel sanmi <sossesam@gmail.com>
2023-04-11 05:59:30 +0100

    1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * string_nconcat - function that concatenates two strings
  * @s1: string one being passed
@@ -9,30 +8,44 @@
  * @n: size
  * Return: Always 0.
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	unsigned int a, b, c, d;
+char *s3;
+unsigned int a, b, c, d;
 
-	if (s1 == 0)
-		s1 = "";
-	if (s2 == 0)
-		s2 = "";
-	for (a = 0; s1[a] != '\0'; a++)
-		;
-	for (b = 0; s2[b] != '\0'; b++)
-		;
-	if (n > b)
-		p = malloc((a + b + 1) * sizeof(char));
-	else
-		p = malloc((a + n + 1) * sizeof(char));
-	if (p == 0)
-		return (NULL);
+if (s2 == NULL)
+{
+s2 = "";
+}
+if (s1 == NULL)
+{
+s1 = "";
+}
+for (a = 0; s1[a] != '\0'; a++)
+;
 
-	for (c = 0; c < a; c++)
-		p[c] = s1[c];
-	for (d = 0; d < n && d < b; d++, c++)
-		p[c] = s2[d];
-	p[c] = '\0';
-	return (p);
+for (b = 1; s2[b] != '\0'; b++)
+;
+
+if (n > b)
+s3 = malloc((a + b + 1) * sizeof(char));
+else
+s3 = malloc((a + n + 1) * sizeof(char));
+if (s3 == NULL)
+{
+return (NULL);
+free(s3);
+}
+
+for (c = 0; c < a; c++)
+s3[c] = s1[c];
+
+for (d = 0; d < n && d < b ; d++, c++)
+{
+s3[c] = s2[d];
+}
+
+s3[c] = '\0';
+return (s3);
 }
