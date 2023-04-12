ffec0e89e68da02f5ed394a3e4140651818f8fed
oyeniran samuel sanmi <sossesam@gmail.com>
2023-04-12 06:38:44 +0100

    all

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -5,45 +5,47 @@
 
 /**
  * _realloc - reallocates a new set of memory
- * old_size: previous allocated memory
- * new_size: newly allocated memory
+ * @old_size: previous allocated memory
+ * @new_size: newly allocated memory
+ * @ptr: pointer to the previous address
  *
  * Return: Nothing.
  */
 
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size){
-    unsigned int a;
-	char *new;
-	char *old;
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+unsigned int a;
+char *new;
+char *old;
 
-    old = ptr;
+old = ptr;
 
-    if (old_size == new_size)
-	return (ptr);
+if (old_size == new_size)
+return (ptr);
 
-    if (ptr == 0)
-	{
-		new = malloc(new_size * sizeof(char));
-		return (new);
-	}
-
-    if (new_size == 0 && ptr != 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
-    new = malloc(new_size * sizeof(char));
-    if (new == 0)
-	return (NULL);
-
-    if (new_size < old_size)
-	{
-		for (a = 0; a < new_size; a++)
-			new[a] = old[a];
-		return (new);
-	}
-
-    free(ptr);
+if (ptr == 0)
+{
+	new = malloc(new_size * sizeof(char));
 	return (new);
+}
+
+if (new_size == 0 && ptr != 0)
+{
+	free(ptr);
+	return (NULL);
+}
+new = malloc(new_size * sizeof(char));
+if (new == 0)
+return (NULL);
+
+if (new_size < old_size)
+{
+for (a = 0; a < new_size; a++)
+	new[a] = old[a];
+	return (new);
+}
+
+free(ptr);
+return (new);
 
 }
