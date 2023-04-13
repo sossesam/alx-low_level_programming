967fcb8a057519ab891efd81d50506806cd07eeb
oyeniran samuel sanmi <sossesam@gmail.com>
2023-04-13 06:33:45 +0100

    all

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-int main()
+
+
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-    printf("%s\n", __FILE__);
-    return 0;
+printf("%s\n", __FILE__);
+return (0);
 }
