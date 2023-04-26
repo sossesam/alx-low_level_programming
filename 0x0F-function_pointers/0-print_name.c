b8c18c39cb940fe3824068a21e2648ee4382b8fe
oyeniran samuel sanmi <sossesam@gmail.com>
2023-04-26 08:29:17 +0100

    all

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
-void print_name(char *name, void (*f)(char *)){
-    (*f)(name);
+void print_name(char *name, void (*f)(char *))
+{
+if(f != NULL)
+{
+f(name);
+}
+
 }
