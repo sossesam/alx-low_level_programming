a7d7440d8f99d8bed7860305eb76927ed86ca6d1
oyeniran samuel sanmi <sossesam@gmail.com>
2023-04-27 06:01:23 +0100

    all

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,24 +2,33 @@
 #include "function_pointers.h"
 
 
-int int_index(int *array, int size, int (*cmp)(int)){
-    int i, c;
-    i = 0;
-    if(size <= 0){
-        return -1;
-    }else
-   {while((*cmp)(array[i]) == 0){
-    if(i >= size){
-        return -1;
-    }
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i, c;
+i = 0;
+if (array != NULL)
+{
+if (cmp != NULL)
+{
+if (size <= 0)
+{
+return (-1);
+}
+else
+{
+while ((*cmp)(array[i]) == 0)
+{
+if (i >= size)
+{
+return (-1);
+}
+i++;
+c = i;
 
-    i++;
-    c = i;
-   
-   }
-   }
-   
-return c;
+}
 }
 
-
+return (c);
+}
+}
+}
