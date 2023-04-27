870748c961fe6e2406205dc3476a1a24063f2778
oyeniran samuel sanmi <sossesam@gmail.com>
2023-04-27 06:53:47 +0100

    all

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -6,7 +6,7 @@ int op_add(int a, int b){
 }
 
 int op_sub(int a, int b){
-    return (a -b);
+    return (a - b);
 }
 
 int op_mul(int a, int b){
@@ -14,6 +14,10 @@ int op_mul(int a, int b){
 }
 
 int op_div(int a, int b){
+    if (a == 0 || b == 0)
+    {
+        return (0);
+    }
     return (a / b);
 }
 
