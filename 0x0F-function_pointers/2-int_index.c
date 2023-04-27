9a28617d288d55d7d13754f17302ea832465f299
oyeniran samuel sanmi <sossesam@gmail.com>
2023-04-27 06:08:52 +0100

    added a return statement

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,6 +2,14 @@
 #include "function_pointers.h"
 
 
+/**
+ * int_index - check if a number is equal to 98
+ * @array: the integer to check
+ * @size:size of array
+ * @cmp: function pointer
+ * Return: -1 if false, something else otherwise.
+ */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i, c;
