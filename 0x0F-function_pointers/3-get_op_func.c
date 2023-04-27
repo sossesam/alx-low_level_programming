9e28a99b92cebbf9f65edbf907190f2da0960523
oyeniran samuel sanmi <sossesam@gmail.com>
2023-04-27 07:43:44 +0100

    all

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,24 +1,29 @@
 #include "3-calc.h"
 #include <stdlib.h>
+/**
+ * get_op_func - function
+ * @s:nxxnxn
+ * Return: result
+ */
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i;
+i = 0;
+while (i < 5)
+{
+if (*(ops[i].op) == *s)
+return (ops[i].f);
 
-int (*get_op_func(char *s))(int, int){
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
-    i = 0;
-    while(i < 5){
-        if(*(ops[i].op) == *s){
-            return ops[i].f;
-        }
-        i++;
-    }
-    
-    i = 0;
-	return (0);
+i++;
+}
+i = 0;
+return (0);
 }
