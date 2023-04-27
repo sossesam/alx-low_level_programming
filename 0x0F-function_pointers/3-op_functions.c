d4ec1cb1c22185d313514f235e651bccfe43d3e7
oyeniran samuel sanmi <sossesam@gmail.com>
2023-04-27 07:34:05 +0100

    all

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,30 +2,69 @@
 #include "3-calc.h"
 #include <stdlib.h>
 
-int op_add(int a, int b){
-    return (a + b);
+
+/**
+ * op_add - function
+ * @a: number of arguments
+ * @b: argument variables passed
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+return (a + b);
 }
 
-int op_sub(int a, int b){
-    return (a - b);
+/**
+ * op_sub - function
+ * @a: number of arguments
+ * @b: argument variables passed
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
 }
 
-int op_mul(int a, int b){
-    return (a * b);
+/**
+ * op_mul - function
+ * @a: number of arguments
+ * @b: argument variables passed
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
 }
 
-int op_div(int a, int b){
-    if(a == 0 || b== 0){
-    printf("Error\n");
-    exit(100);
-    }
-    return (a / b);
+/**
+ * op_div - function
+ * @a: number of arguments
+ * @b: argument variables passed
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+if (a == 0 || b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a / b);
 }
 
-int op_mod(int a, int b){
-    if(a == 0 || b== 0){
-    printf("Error\n");
-    exit(100);
-    }
-    return (a % b);
+/**
+ * op_mod - function
+ * @a: number of arguments
+ * @b: argument variables passed
+ * Return: result
+ */
+
+int op_mod(int a, int b)
+{
+if (a == 0 || b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a % b);
 }
