3d3c28a0dbe2fd1aa5ed86cd6d88671ccaf35713
oyeniran samuel sanmi <sossesam@gmail.com>
2023-04-28 07:05:13 +0100

    all

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void){
-    FILE *fp;
-    char c;
+/**
+ * main - function to print its own opcode
+ * @argc: number of arguments
+ * @argv: argument variables passed
+ * Return: Always 0/
+ */
+int main(int argc, char **argv)
+{
+	int a, b;
+	char *s;
 
-    fp = fopen(__FILE__,"r");
-
-    while ((c = getc(fp)) != EOF)
-        printf("%02hhx ", c);
-
-    fclose(fp);
-    return 0;
+	if (argc < 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	a = atoi(argv[1]);
+	if (a < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	s = (char *)main;
+	for (b = 0; b < a - 1; b++)
+		printf("%02hhx ", s[b]);
+	printf("%02hhx\n", s[b]);
+	return (0);
 }
