8d664b4e9cf00d079edf2bf97237aa779453ddf0
oyeniran samuel sanmi <sossesam@gmail.com>
2023-05-21 23:02:05 +0100

    all

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,15 @@
  * Return: count of the nodes
  */
 
-size_t print_listint(const listint_t *h){
+size_t print_listint(const listint_t *h)
+{
 size_t count;
 
 count = 0;
-while (h != NULL){
+while (h != NULL)
+{
 
-printf("%d\n", h->n );
+printf("%d\n", h->n);
 h = h->next;
 count++;
 
