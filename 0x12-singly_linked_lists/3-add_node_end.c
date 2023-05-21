c5892e113917a315994d038ac66d595b8b73cac2
oyeniran samuel sanmi <sossesam@gmail.com>
2023-05-21 09:46:34 +0100

    all

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,6 +4,12 @@
 #include <stddef.h>
 #include "lists.h"
 
+/**
+ * add_node_end - adds a node to the ennd of a list
+ * @head: start of a list
+ * @str: a string
+ * Return: return the new list
+*/
 
 list_t *add_node_end(list_t **head, const char *str)
 {
