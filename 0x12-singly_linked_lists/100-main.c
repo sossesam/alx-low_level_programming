52bef1f618d459e3ab9d781474377c031819e3e2
oyeniran samuel sanmi <sossesam@gmail.com>
2023-05-21 14:07:30 +0100

    all

---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+    before_main();
+    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+    return (0);
+}
