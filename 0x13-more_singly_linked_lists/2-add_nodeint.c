a16363cebd3e3b127e1eb95580a30fbb2da21ca4
oyeniran samuel sanmi <sossesam@gmail.com>
2023-05-23 06:40:55 +0100

    all

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -6,7 +6,7 @@
  * add_nodeint- add a node to the beginning
  * @head: start of the node
  * @n:  number added
- * Returns: return the address to the head
+ * Return: return the address to the head
 */
 
 listint_t *add_nodeint(listint_t **head, const int n)
@@ -14,8 +14,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 listint_t *new;
 
 new = malloc(sizeof(listint_t));
-if(new == NULL)
-    return (NULL);
+if (new == NULL)
+return (NULL);
 
 new->n = n;
 new->next = *head;
