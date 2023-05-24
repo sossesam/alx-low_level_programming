1196c536dd80809c14928ad9963d23578baedc67
oyeniran samuel sanmi <sossesam@gmail.com>
2023-05-24 05:54:58 +0100

    all

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,6 +5,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 unsigned int count;
 count = 0;
+if (head == NULL)
+{
+return (NULL);
+}
 while (head->next != NULL && count < index)
 {
 head = head->next;
