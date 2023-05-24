e542789620b068c63e63839e2a08087a9b550726
oyeniran samuel sanmi <sossesam@gmail.com>
2023-05-24 08:49:44 +0100

    all

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,44 +9,44 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new, *old, *tmp;
-	unsigned int i = 1;
+listint_t *new, *old, *tmp;
+unsigned int i = 1;
 
-	if (head == NULL)
-		return (NULL);
+if (head == NULL)
+return (NULL);
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = NULL;
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+return (NULL);
+new->n = n;
+new->next = NULL;
 
-	if (idx == 0)
-	{
-		new->next = *head;
-		*head = new;
-		return (new);
-	}
-    
-	old = *head;
-	tmp = (*(head))->next;
-	while (tmp != NULL)
-	{
-		if (i == idx)
-		{
-			old->next = new;
-			new->next = tmp;
-			return (new);
-		}
-		old = tmp;
-		tmp = tmp->next;
-		i++;
-	}
-	if (tmp == NULL && i == idx)
-	{
-		old->next = new;
-		return (new);
-	}
-	free(new);
-	return (NULL);
+if (idx == 0)
+{
+new->next = *head;
+*head = new;
+return (new);
+}
+
+old = *head;
+tmp = (*(head))->next;
+while (tmp != NULL)
+{
+if (i == idx)
+{
+old->next = new;
+new->next = tmp;
+return (new);
+}
+old = tmp;
+tmp = tmp->next;
+i++;
+}
+if (tmp == NULL && i == idx)
+{
+old->next = new;
+return (new);
+}
+free(new);
+return (NULL);
 }
