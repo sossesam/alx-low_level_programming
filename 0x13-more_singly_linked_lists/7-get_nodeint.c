766c28ee9664cc0f954ff09e9d88aab2b7ac2321
oyeniran samuel sanmi <sossesam@gmail.com>
2023-05-24 05:57:22 +0100

    all

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
-
+/**
+ * get_nodeint_at_index - get the node at an index
+ * @head: the start of node
+ * @index: position of node
+ * Return: returns node
+*/
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
