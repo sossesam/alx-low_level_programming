5f61f2a74da4ea527efa34a425ec56ece02736bf
oyeniran samuel sanmi <sossesam@gmail.com>
2023-05-24 07:03:02 +0100

    all

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,17 +1,23 @@
 #include "lists.h"
 
+/**
+ * sum_listint- adds up all the sum of numbers
+ * @head: the node
+ * Return: return the sum of integers
+*/
+
 int sum_listint(listint_t *head)
 {
-    int sum, num;
-    sum = 0;
+int sum, num;
+sum = 0;
 
 
-    while(head != NULL)
-    {
-        num = head->n;
-        sum = sum + num;
-        head = head->next;
+while(head != NULL)
+{
+num = head->n;
+sum = sum + num;
+head = head->next;
 
-    }
-    return (sum);
+}
+return (sum);
 }
