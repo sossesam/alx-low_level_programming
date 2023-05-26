f3efef3631b7c73cf1e69993f23a6e46df9a69b5
oyeniran samuel sanmi <sossesam@gmail.com>
2023-05-26 23:29:12 +0100

    all

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,28 @@
 #include "main.h"
 #include <stdio.h>
 
-
+/**
+ * binary_to_uint- converts binary to decimal
+ * @b: pointer to character
+ * Return: returns the decimal
+*/
 
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int res;
-    res = 0;
-    if (b == NULL)
-    return (0);
+unsigned int res;
+res = 0;
+if (b == NULL)
+return (0);
 
 
-    while (*b != '\0')
-    {
-        if(*b != '0' && *b != '1')
-        return(0);
+while (*b != '\0')
+{
+if (*b != '0' && *b != '1')
+return (0);
 
-        res = (res << 1) +  *b - '0';
-        b++;
-    }
+res = (res << 1) +  *b - '0';
+b++;
+}
 return (res);
 
 }
