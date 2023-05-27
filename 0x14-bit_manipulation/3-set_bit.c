7d3b7fc7b663248078be008b5e487619a4c905d0
oyeniran samuel sanmi <sossesam@gmail.com>
2023-05-27 15:32:55 +0100

    all

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+unsigned int i;
+int a;
+i = 0;
+if ((sizeof(unsigned long int) * 8) < index)
+return (-1);
+
+while (i < index)
+{
+n = n >> 1;
+i++;
+}
+
+a = n & 1;
+if (i <= index)
+return (a);
+
+return (a);
+}
+
+
+int set_bit(unsigned long int *n, unsigned int index)
+{unsigned long int check, temp;
+
+temp = 1;
+check = get_bit(*n, index);
+if(!check){
+temp = temp << index;
+}
+*n = *n + temp;
+return (*n);
+}
