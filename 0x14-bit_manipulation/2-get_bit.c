ab3095c47c5cac3ae3fd7fcdc87bdec04aa78a5f
oyeniran samuel sanmi <sossesam@gmail.com>
2023-05-27 08:17:48 +0100

    all

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,21 +1,26 @@
 #include "main.h"
-
+/**
+ * get_bit- gets the nth bit
+ * @n: number
+ * @index: position of the bit
+ * Return: returns the bit
+*/
 int get_bit(unsigned long int n, unsigned int index)
 {
 unsigned int i;
 int a;
 i = 0;
-if((sizeof(unsigned long int) * 8) < index)
+if ((sizeof(unsigned long int) * 8) < index)
 return (-1);
 
-while(i < index)
+while (i < index)
 {
 n = n >> 1;
 i++;
 }
 
 a = n & 1;
-if(i <= index)
+if (i <= index)
 return (a);
 
 return (a);
