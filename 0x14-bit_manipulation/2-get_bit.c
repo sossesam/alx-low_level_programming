4a458e8d5083177e5babb772ce2de21357cfcf10
oyeniran samuel sanmi <sossesam@gmail.com>
2023-05-27 02:34:11 +0100

    ALL

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+int get_bit(unsigned long int n, unsigned int index)
+{unsigned int i;
+int a;
+i = 0;
+
+while(i < index)
+{
+    n = n >> 1;
+    i++;
+}
+a = n & 1;
+return (a);
+}
