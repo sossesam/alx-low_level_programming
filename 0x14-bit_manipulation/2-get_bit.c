34b97205717301a8dc823c9b7f5a1d8a7697a2f1
oyeniran samuel sanmi <sossesam@gmail.com>
2023-05-27 08:14:30 +0100

    all

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 int get_bit(unsigned long int n, unsigned int index)
-{unsigned int i;
+{
+unsigned int i;
 int a;
 i = 0;
-if((sizeof(unsigned long int) * 8) < index){
-    return (-1);
-}
+if((sizeof(unsigned long int) * 8) < index)
+return (-1);
 
 while(i < index)
 {
-    n = n >> 1;
-    i++;
+n = n >> 1;
+i++;
 }
+
 a = n & 1;
-if( i <= index){
-    return (a);
-}
+if(i <= index)
+return (a);
+
 return (a);
 }
