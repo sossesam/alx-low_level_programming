37f035028e97a72f49fd0cac309e6e1f0df86f5a
oyeniran samuel sanmi <sossesam@gmail.com>
2023-05-29 14:43:13 +0100

    all

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,7 +3,7 @@
 #include <fcntl.h>
 
 /**
- * ssize_t read_textfile- reads a file
+ * read_textfile- reads a file
  * @filename: address of the file
  * @letters: number of bytes
  *
@@ -15,24 +15,24 @@ char *buf = NULL;
 int fd;
 ssize_t bytes;
 
-if(!*filename || !letters)
+if (!*filename || !letters)
 return (0);
 
 buf = malloc(letters + 1);
-if(buf == NULL)
+if (buf == NULL)
 return (0);
 
 fd = open(filename, O_RDONLY);
-if(fd == -1)
+if (fd == -1)
 return (free(buf), 0);
 
 bytes = read(fd, buf, letters);
 if (bytes == -1)
 return (free(buf), 0);
 
-buf[letters]='\0';
+buf[letters] = '\0';
 bytes = write(STDOUT_FILENO, buf, bytes);
-if(bytes == -1)
+if (bytes == -1)
 return (free(buf), 0);
 
 free(buf);
