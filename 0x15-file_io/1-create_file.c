1a0135768df75ce2aafbce6f0ca76cefe89f7070
oyeniran samuel sanmi <sossesam@gmail.com>
2023-05-30 09:29:07 +0100

    all

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,31 @@
 #include "main.h"
 
+/**
+ * create_file - function to create file
+ * @filename: pointer to the file name to be created
+ * @text_content: str to be written in the file
+ * Return: 1 success -1 fail
+ */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, output, strlen;
-    if(filename == NULL)
-    return (-1);
+	int input, output, count = 0;
 
+	if (filename == NULL)
+		return (-1);
+	input = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
-    fd = open(filename, O_RDWR|O_CREAT|  O_TRUNC, 0600);
-    if (fd == -1)
-        return (-1);
+	if (input == -1)
+		return (-1);
+	if (text_content == NULL)
+		return (1);
 
-    if(text_content == NULL)
-        return (1);
+	for (; text_content[count] != '\0'; count++)
+		;
 
-    strlen = 0;
-    while( *text_content != '\0')
-    {
-        strlen++;
-        text_content++;
-    }
-
-    output = write(fd, text_content, strlen);
-
-    if (output == -1 || output != strlen)
+	output = write(input, text_content, count);
+	if (output == -1 || output != count)
 		return (-1);
 
-	close(fd);
+	close(input);
 	return (1);
-
-
 }
-
