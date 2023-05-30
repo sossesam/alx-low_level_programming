6a6139d5f2f2e737828252a8f47deee01b1963a5
oyeniran samuel sanmi <sossesam@gmail.com>
2023-05-30 09:34:03 +0100

    all

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,6 @@ int create_file(const char *filename, char *text_content)
     while( text_content[strlen] != '\0')
     {
         strlen++;
-        text_content++;
     }
 
     output = write(fd, text_content, strlen);
