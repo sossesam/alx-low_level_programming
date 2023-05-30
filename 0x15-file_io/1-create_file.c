952ca0f22255520925621d90e338bced7fcb6bd1
oyeniran samuel sanmi <sossesam@gmail.com>
2023-05-30 09:39:37 +0100

    all

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,32 +9,32 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    int fd, output, strlen;
+int fd, output, strlen;
 
-    if(filename == NULL)
-    return (-1);
+if (filename == NULL)
+return (-1);
 
 
-    fd = open(filename, O_RDWR|O_CREAT|  O_TRUNC, 0600);
-    if (fd == -1)
-        return (-1);
+fd = open(filename, O_RDWR | O_CREAT |  O_TRUNC, 0600);
+if (fd == -1)
+return (-1);
 
-    if(text_content == NULL)
-        return (1);
+if (text_content == NULL)
+return (1);
 
-    strlen = 0;
-    while( text_content[strlen] != '\0')
-    {
-        strlen++;
-    }
+strlen = 0;
+while (text_content[strlen] != '\0')
+{
+strlen++;
+}
 
-    output = write(fd, text_content, strlen);
+output = write(fd, text_content, strlen);
 
-    if (output == -1 || output != strlen)
-		return (-1);
+if (output == -1 || output != strlen)
+return (-1);
 
-	close(fd);
-	return (1);
+close(fd);
+return (1);
 
 
 }
