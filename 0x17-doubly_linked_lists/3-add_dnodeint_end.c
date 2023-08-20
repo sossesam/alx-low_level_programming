776b66f38f519b7da04ccda2ed3894be3f1cab79
oyeniran samuel sanmi <sossesam@gmail.com>
2023-08-20 23:17:24 +0100

    all

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -4,7 +4,7 @@
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 
-dlistint_t *temp = malloc(sizeof(dlistint_t));
+dlistint_t *temp;
 dlistint_t *new_node = malloc(sizeof(dlistint_t));
 if (new_node == NULL)
 {
