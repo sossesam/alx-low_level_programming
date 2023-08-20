27d1f2e75b94b0d227c983d0c0a2547071248e53
oyeniran samuel sanmi <sossesam@gmail.com>
2023-08-20 08:53:40 +0100

    all

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,17 +1,22 @@
 #include "lists.h"
 
+/**
+ * dlistint_len- returns the number of nodes in alist
+ * @h: the head of the node
+ * Return: return the number of node
+*/
 size_t dlistint_len(const dlistint_t *h)
 {
-    const dlistint_t *head;
-    size_t number_of_nodes;
-    head = h;
-    number_of_nodes = 0;
+const dlistint_t *head;
+size_t number_of_nodes;
+head = h;
+number_of_nodes = 0;
 
-    while (head != NULL)
-    {
-        number_of_nodes++;
-        head = head->next;
-    }
+while (head != NULL)
+{
+number_of_nodes++;
+head = head->next;
+}
 
-return number_of_nodes;
+return (number_of_nodes);
 }
