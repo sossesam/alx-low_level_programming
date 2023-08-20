419f3310bb4d87570e6e7e9aebc43d10313120fc
oyeniran samuel sanmi <sossesam@gmail.com>
2023-08-20 08:27:59 +0100

    all

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,20 +1,25 @@
 #include "lists.h"
 
-size_t print_dlistint(const dlistint_t *h){
+
+/**
+ * print_dlistint- prints a list of nodes
+ * @h: the head of the node
+ * Return: returns the number of nodes
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
 size_t number_of_nodes;
 
 const dlistint_t *head;
 
 head = h;
 number_of_nodes = 0;
-while(head != NULL)
+while (head != NULL)
 {
-    printf("%d\n",head->n);
-    head = head->next;
-    number_of_nodes++;
+printf("%d\n", head->n);
+head = head->next;
+number_of_nodes++;
 }
 
-
-
-return number_of_nodes;
+return (number_of_nodes);
 }
