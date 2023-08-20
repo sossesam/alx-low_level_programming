3bc758cbcf90e99953b120441d4fd07929790dfd
oyeniran samuel sanmi <sossesam@gmail.com>
2023-08-20 20:14:31 +0100

    all

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,19 +1,41 @@
 #include "lists.h"
 
+
+/*start program
+create a new node
+if creation fails
+    free new_node
+    return NULL
+
+set new_node.n as n
+set new_node.prev as NULL;
+set new_node.next as head
+set the head.prev as new_node
+
+
+set head as new_node;
+*/
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));
-
-    new_node->n = n;
-    new_node->prev = NULL;
-    new_node->next = NULL;
-
-    if (head != NULL)
-    {
-        new_node->next = *head;
-    }
-    *head = new_node;
+dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
 
-    return *head;
+
+new_node->n = n;
+new_node->prev = NULL;
+new_node->next = *head;
+
+
+if(*head != NULL)
+{
+(*head)->prev  = new_node;
+}
+
+
+*head = new_node;
+
+
+
+
+return *head;
 }
