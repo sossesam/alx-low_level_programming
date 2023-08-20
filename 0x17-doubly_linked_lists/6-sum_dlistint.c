6a34daa5a7c37bef24c85e90c904682093765aee
oyeniran samuel sanmi <sossesam@gmail.com>
2023-08-21 00:19:27 +0100

    all

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * sum_dlistint- adds the values of node in a list
+ *  @head: start of the list
+ * Return: returns the sum of the nodes
+*/
 
 
 int sum_dlistint(dlistint_t *head)
@@ -7,12 +11,12 @@ int sum_dlistint(dlistint_t *head)
 int sum;
 sum = 0;
 if (head == NULL)
-return sum;
+return (sum);
 
-while(head != NULL)
+while (head != NULL)
 {
 sum = head->n + sum;
 head = head->next;
 }
-return sum;
+return (sum);
 }
