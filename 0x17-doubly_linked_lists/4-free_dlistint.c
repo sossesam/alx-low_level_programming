ada7c823d2c4a83588668f8373c877bbb783d93d
oyeniran samuel sanmi <sossesam@gmail.com>
2023-08-20 23:34:03 +0100

    all

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,14 +1,19 @@
 #include "lists.h"
 
+/**
+ * free_dlistint- free's a list
+ * @head: start of the list
+ * Return: returns void
+*/
 
 
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *temp;
-    while(head != NULL)
-    {
-        temp = head->next;
-        free(head);
-        head = temp;
-    }
+dlistint_t *temp;
+while (head != NULL)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
 }
