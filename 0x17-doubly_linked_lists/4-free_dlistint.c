fcceac501e079672a5079f0077613f28854a1528
oyeniran samuel sanmi <sossesam@gmail.com>
2023-08-20 23:30:09 +0100

    all

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+
+
+void free_dlistint(dlistint_t *head)
+{
+    dlistint_t *temp;
+    while(head != NULL)
+    {
+        temp = head->next;
+        free(head);
+        head = temp;
+    }
+}
