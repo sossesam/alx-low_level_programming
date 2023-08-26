70a72509c10f2ff3ede221e7728f26815fcca504
sossesam <sossesam@gmail.com>
2023-08-26 11:11:54 -0700

    all

---
+++ 0x18-dynamic_libraries/100-operations/100.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+
+
+// function to add two integer numbers
+int add(int n1, int n2)
+{
+  int result;
+  result = n1 + n2;
+  return result;
+}
+
+// function to subtract two integer numbers
+int sub(int n1, int n2)
+{
+  int result;
+  result = n1 - n2;
+  return result;
+}
+
+// function to multiply two integer numbers
+int mul(int n1, int n2)
+{
+  int result;
+  result = n1 * n2;
+  return result;
+}
+
+// function to divide two integer numbers
+int div(int n1, int n2)
+{
+  int result;
+  result = n1 / n2;
+  return result;
+}
+
+int mod(int n1, int n2)
+{
+  int result;
+  result = n1 % n2;
+  return result;
+}
