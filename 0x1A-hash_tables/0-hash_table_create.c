b3d23e8f4a6380e3bae1572e7c4d7c5c07d743b7
sossesam <sossesam@gmail.com>
2024-01-14 15:15:53 +0100

    all

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+hash_table_t *table;
+table = malloc(sizeof(hash_table_t) * size);
+
+return table;
+
+}
