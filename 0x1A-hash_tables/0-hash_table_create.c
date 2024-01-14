8926c6891afe6991e1e09cc57c56c63883786055
sossesam <sossesam@gmail.com>
2024-01-14 15:41:10 +0100

    all

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,11 +4,21 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *table;
-table = malloc(sizeof(hash_table_t) * size);
+table = NULL;
+if(size == 0){
+    return NULL;
+}
+table = malloc(sizeof(hash_table_t));
 if(table == NULL){
-return -1;
+    return NULL;
+}
+
+table->size = size;
+table->array = malloc(sizeof(hash_node_t) * size);
+
+if(table->array == NULL){
+    return NULL;
 }
 
 return table;
-
 }
