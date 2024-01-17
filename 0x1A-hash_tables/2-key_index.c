5180d9803cda7b1f49becf7017e3273451e5b6b9
sossesam <sossesam@gmail.com>
2024-01-17 08:23:22 +0100

    all

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - function to return index of a key
+ * @key: key to get index
+ * @size: size of array of hash table
+ * Return: index based on hash function of key
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+    unsigned long int hash_key = hash_djb2(key) % size;
+    return hash_key;
+}
