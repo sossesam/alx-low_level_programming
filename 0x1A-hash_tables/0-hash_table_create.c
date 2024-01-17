b6588dfa696a849d4aab6508eeadca9eb1472b99
sossesam <sossesam@gmail.com>
2024-01-17 07:58:28 +0100

    all

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /*
-* hash_table_create- creates a hash table;
+* hash_table_create - creates a hash table;
 * @size: size of the table;
 *
 *Return: returns the address to the table;
@@ -11,18 +11,23 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *table;
+
 table = NULL;
+
 if(size == 0)
 {
 return NULL;
 }
+
 table = malloc(sizeof(hash_table_t));
+
 if(table == NULL)
 {
 return NULL;
 }
 
 table->size = size;
+
 table->array = malloc(sizeof(hash_node_t *) * size);
 
 if(table->array == NULL)
