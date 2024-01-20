41d4e3938747bdd1578c7268ca586689b4bd2391
sossesam <sossesam@gmail.com>
2024-01-20 14:22:07 +0100

    all

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -4,24 +4,23 @@
 char *hash_table_get(const hash_table_t *ht, const char *key){
     const hash_node_t *table;
     unsigned long int index_number;
+
+    if(ht == NULL)
+    {
+        
+        return NULL;
+    }
    
 
     index_number= key_index((const unsigned char *)key, ht->size);
     table = ht->array[index_number];
 
-    if(table == NULL)
-    {
-        return NULL;
-    }
+    
 
     while(table != NULL)
     {
         if(strcmp(table->key, key) == 0)
         {
-            return table->value;
-        }
-
-        if(table->key == NULL){
             return table->value;
         }
         table = table->next;
