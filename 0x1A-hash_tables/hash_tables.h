f714e1a723259bb34d74bff7fddc961fff78c7f7
sossesam <sossesam@gmail.com>
2024-01-20 15:28:12 +0100

    all

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -43,4 +43,5 @@ hash_node_t *_node_(const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 char *strdup(const char *c);
 void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 #endif
